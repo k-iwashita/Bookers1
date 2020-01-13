c2876a3cd06de1e800173f628955fc41d7e705d0
k-iwashita <k.iwashita.gude@gmail.com>
2020-01-14 01:35:27 +0900

    機能の完成（CSSはまだ）

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      flash[:error_messages] = @book.errors.full_messages
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to @book
+    else
+      flash[:error_messages] = @book.errors.full_messages
+      render :edit
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def home
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root 'books#home'
+  resources :books
+end
